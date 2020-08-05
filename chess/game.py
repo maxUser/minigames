86a2f3f15c16f3ce979574b3640793ed0a75f5c1
Max Kasprzik <m.kasprzik04@gmail.com>
2020-08-05 15:18:42 -0700

    separated some functionality

--- chess/game.py
+++ chess/game.py
@@ -13,19 +13,6 @@ def pawn_rules(colour, curr_pos, tar_pos):
     curr_x_y = [letter_to_number(curr_pos[0]), y_flip(curr_pos[1])]
     tar_x_y = [letter_to_number(tar_pos[0]), y_flip(tar_pos[1])]
 
-    # Can only move forward by 1 square unless moving from
-    # starting position, then can move 1 or 2 squares forward
-    if not board.squares[curr_pos].initial_pos:
-        if curr_x_y[1] - tar_x_y[1] > 1 and colour == 'cyan':
-            return 'illegal'
-        elif curr_x_y[1] - tar_x_y[1] < -1 and colour == 'yellow':
-            return 'illegal'
-    else:
-        if curr_x_y[1] - tar_x_y[1] > 2 and colour == 'cyan':
-            return 'illegal'
-        elif curr_x_y[1] - tar_x_y[1] < -2 and colour == 'yellow':
-            return 'illegal'
-
     # Cannot move backwards
     if curr_x_y[1] - tar_x_y[1] <= 0 and colour == 'cyan':
         return 'illegal'
@@ -36,8 +23,24 @@ def pawn_rules(colour, curr_pos, tar_pos):
     if tar_x_y[0] == curr_x_y[0] and board.squares[tar_pos] != '':
         return 'illegal'
 
+    # Can only move forward by 1 square unless moving from
+    # starting position, then can move 1 or 2 squares forward
+    if not board.squares[curr_pos].initial_pos:
+        if curr_x_y[1] - tar_x_y[1] > 1 and colour == 'cyan':
+            return 'illegal'
+        elif curr_x_y[1] - tar_x_y[1] < -1 and colour == 'yellow':
+            return 'illegal'
+    elif board.squares[curr_pos].initial_pos:
+        if curr_x_y[1] - tar_x_y[1] > 2 and colour == 'cyan':
+            return 'illegal'
+        elif curr_x_y[1] - tar_x_y[1] < -2 and colour == 'yellow':
+            return 'illegal'
+
     # Take piece diagonally
-    elif tar_x_y[0] != curr_x_y[0] and board.squares[tar_pos] != '' and curr_x_y[1] - tar_x_y[1] == 1 and colour == 'cyan':
+    print(tar_x_y[0], curr_x_y[0])
+    print(board.squares[tar_pos])
+    print(curr_x_y[1] - tar_x_y[1] == 1)
+    if tar_x_y[0] != curr_x_y[0] and board.squares[tar_pos] != '' and curr_x_y[1] - tar_x_y[1] == 1 and colour == 'cyan':
         return 'take'
     elif tar_x_y[0] != curr_x_y[0] and board.squares[tar_pos] != '' and curr_x_y[1] - tar_x_y[1] == -1 and colour == 'yellow':
         return 'take'
@@ -45,10 +48,11 @@ def pawn_rules(colour, curr_pos, tar_pos):
 
     return 'move'
 
-def check_move2(team, curr_pos, tar_pos):
+def check_move(team, curr_pos, tar_pos):
     colour = team.colour
     oppo_team = get_oppo_team(colour)
 
+    # check if input matches a valid board square
     if curr_pos not in board.squares or tar_pos not in board.squares:
         return 'invalid_input'
 
@@ -56,7 +60,16 @@ def check_move2(team, curr_pos, tar_pos):
     if board.squares[curr_pos] == '' or colour != board.squares[curr_pos].team:
         return 'selection_error'
 
-    # Check if move is legal according to piece move restrictions
+    # check if piece move through another piece (unless knight)
+    # if board.squares[curr_pos].type != 'knight':
+
+
+    # friendly fire is off
+    if board.squares[curr_pos] == '' and board.squares[tar_pos] == '':
+        if board.squares[tar_pos].team == board.squares[curr_pos].team:
+            return 'ff'
+
+    # check if move is legal according to piece move restrictions
     if board.squares[curr_pos].type == 'pawn':
         return pawn_rules(colour, curr_pos, tar_pos)
 
@@ -67,14 +80,8 @@ def check_move2(team, curr_pos, tar_pos):
         """
         return 'move'
 
-    # friendly fire is off
-    if board.squares[tar_pos].team == board.squares[curr_pos].team:
-        return 'ff'
 
-
-
-
-def check_move(team, curr_x_y, new_x_y):
+def check_move_old(team, curr_x_y, new_x_y):
     colour = team.colour
     oppo_team = get_oppo_team(colour)
 
@@ -188,6 +195,12 @@ def print_pieces(team):
 def print_error(msg):
     print('ERROR:\n***\n{}\n***'.format(msg))
 
+def get_piece_to_move():
+    return input('Piece to move: ')
+
+def get_target_position():
+    return input('Move to: ')
+
 def player_move(team):
     """
     curr_pos, tar_pos (str): get player input - this will be
@@ -196,12 +209,11 @@ def player_move(team):
                             chess board.
 
     """
-    board.print_board()
 
     print(colored(team.colour + '\'s turn', team.colour))
 
-    curr_pos = input('Piece to move: ')
-    tar_pos = input('Move to: ')
+    curr_pos = get_piece_to_move()#input('Piece to move: ')
+    tar_pos = get_target_position()#input('Move to: ')
 
 
     if curr_pos == 'pp':
@@ -210,51 +222,62 @@ def player_move(team):
 
     # convert user input to list element indices
     curr_x_y = [letter_to_number(curr_pos[0]), y_flip(curr_pos[1])]
-    new_x_y = [letter_to_number(tar_pos[0]), y_flip(tar_pos[1])]
+    tar_x_y = [letter_to_number(tar_pos[0]), y_flip(tar_pos[1])]
 
 
     # result = check_move(team, curr_x_y, new_x_y)
-    result = check_move2(team, curr_pos, tar_pos)
-    # print('result: ' + result)
+    result = check_move(team, curr_pos, tar_pos)
+    # print('result of check_move is: {}'.format(result))
 
+    return result, curr_pos, tar_pos, curr_x_y, tar_x_y
+
+
+def act_on_result(result, curr_pos, tar_pos, curr_x_y, tar_x_y, team):
+    """
+        Takes result of player_move and check_move functions
+        to determine next action
+
+        Return: 0 if move is invalid
+                1 if move is valid
+    """
+    # print('result is {}'.format(result))
     if result == 'ff':
         print_error('Target square occupied by friendly piece')
-        player_move(team)
+        # player_move(team)
+        return 0
 
     elif result == 'illegal':
         print_error('Illegal move')
-        player_move(team)
+        # player_move(team)
+        return 0
 
     elif result == 'selection_error':
         print_error('{} is an invalid selection'.format(curr_pos))
-        player_move(team)
+        # player_move(team)
+        return 0
 
     elif result == 'invalid_input':
         print_error('Invalid input')
-        player_move(team)
+        # player_move(team)
+        return 0
 
     elif result == 'take':
         print('Taking piece at: ' + tar_pos)
-        board.alter(curr_x_y, new_x_y)
+        board.alter(curr_x_y, tar_x_y)
 
         # add piece to team's graveyard
         team.graveyard.append(board.squares[tar_pos])
         # remove current piece occupying square
         board.squares[tar_pos] = board.squares[curr_pos]
         board.squares[curr_pos] = ''
-
+        return 1
 
     elif result == 'move':
         print('Moving {} to {}'.format(curr_pos, tar_pos))
         board.squares[curr_pos].initial_pos = False
-        board.alter(curr_x_y, new_x_y)
+        board.alter(curr_x_y, tar_x_y)
         board.move_piece(curr_pos, tar_pos)
-
-    return False
-
-def test_1_turn():
-    player_move(cy_team)
-    board.print_board()
+        return 1
 
 def main():
 
@@ -270,8 +293,15 @@ def main():
 
     while win is False:
         print()
-        win = player_move(teams[i%2])
-        i += 1
+        # print('ROUND {}'.format(i+1))
+        # print('i: {}'.format(i))
+        board.print_board()
+
+        team = teams[i%2]
+        move_result, curr_pos, tar_pos, curr_x_y, tar_x_y = player_move(team)
+        # print('result of player_move is {}'.format(move_result))
+        i += act_on_result(move_result, curr_pos, tar_pos, curr_x_y, tar_x_y, team)
+        # i += 1
 
 
 if __name__ == '__main__':
